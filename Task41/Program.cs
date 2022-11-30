4c9ef47e43551ab7951c3f23447ab6fbc551a901
idoctorvic <loukianova@yahoo.com>
2022-11-30 15:55:09 +0300

    Task41 Natural Numbers, string method

---
+++ Task41/Program.cs
@@ -0,0 +1,45 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] IntArray()
+{
+Console.Write("Enter few numbers separated by spaces: ");
+string? sNumbers = Console.ReadLine();
+string[] sArray = sNumbers.Split(" ");
+int[] intArray = Array.ConvertAll<string, int>(sArray, int.Parse);
+return intArray;
+}
+
+int[] result = IntArray();
+PrintArray(result);
+Console.WriteLine(CountNatural(result));
+
+void PrintArray(int[] array)
+{
+	for (int i = 0; i < array.Length; i++)
+	{
+		if (i == array.Length - 1)
+		{
+			Console.WriteLine($"{array[i]}");
+		}
+		else
+		{
+			Console.Write($"{array[i]}, ");
+		}
+	}
+}
+
+int CountNatural(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
